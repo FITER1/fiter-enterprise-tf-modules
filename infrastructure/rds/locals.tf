99a23e2abc9415deb207e59e7636b8f57a43aecf
seyio91 <seyi_obaweya@yahoo.com>
2025-12-16 09:20:03 +0100

    chore: expose rds tag field

--- infrastructure/rds/locals.tf
+++ infrastructure/rds/locals.tf
@@ -1,9 +1,9 @@
 locals {
   publicly_accessible = var.disable_rds_public_access ? false : true
-  tags = {
+  tags = merge({
     Name    = var.db_identifier
     OwnedBy = "Terraform"
-  }
+  }, var.tags)
   security_group_map = { for key in var.allowed_cidrs : key.name => key }
   lambda_layer       = var.engine == "mysql" ? "pymysql.zip" : "psycopg2.zip"
 }
